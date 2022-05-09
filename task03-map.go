c7b1c590ec8747604a0c82a395f9f192ddc1db5a
a.volkovytskaya <a.volkovytskaya@admitad.com>
2022-05-09 15:09:06 +0300

    lecture-03-homework-fix-map-sorting

--- task03-map.go
+++ task03-map.go
@@ -1,8 +1,15 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	for _, v := range input {
-		result = append(result, v)
+	keys := make([]int, 0, len(input))
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		result = append(result, input[k])
 	}
 	return
 }
